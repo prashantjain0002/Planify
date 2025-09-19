b269182a093b22e96cb0d42d6a8287e0b1442a98
Prashant Jain <prashantjain0002@gmail.com>
2025-09-19 14:54:40 +0530

    ⚙️ Setup server with middleware and connect to MongoDB

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(morgan("dev"));
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("🗄️  Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", async (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
