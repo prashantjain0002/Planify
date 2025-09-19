bd56afa38217af6957d29093140eb816a174d380
Prashant Jain <prashantjain0002@gmail.com>
2025-09-19 13:49:17 +0530

    🔑 Create auth routes and pages

--- frontend/app/routes.js
+++ frontend/app/routes.js
@@ -1,3 +1,12 @@
-import { index } from "@react-router/dev/routes";
+import { index, layout, route } from "@react-router/dev/routes";
 
-export default [index("routes/root/Home.jsx")];
+export default [
+  layout("routes/auth/AuthLayout.jsx", [
+    index("routes/root/Home.jsx"),
+    route("sign-in", "routes/auth/SignIn.jsx"),
+    route("sign-up", "routes/auth/SignUp.jsx"),
+    route("forgot-password", "routes/auth/ForgotPassword.jsx"),
+    route("reset-password", "routes/auth/ResetPassword.jsx"),
+    route("verify-email", "routes/auth/VerifyEmail.jsx"),
+  ]),
+];
--- frontend/app/routes/root/Home.jsx
+++ frontend/app/routes/root/Home.jsx
@@ -1,5 +1,6 @@
-import { Button } from '@/components/ui/button';
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
+import { Link } from "react-router";
 
 export function meta({}) {
   return [
@@ -10,8 +11,17 @@ export function meta({}) {
 
 const HomePage = () => {
   return (
-    <div>HomePage <Button>Click</Button></div>
-  )
-}
+    <div className="w-full h-screen flex items-center justify-center gap-5">
+      <Link to="/sign-in">
+        <Button className="bg-blue-500 text-white">Log In</Button>
+      </Link>
+      <Link to="/sign-up">
+        <Button className="bg-blue-500 text-white" variant={"outline"}>
+          Sign Up
+        </Button>
+      </Link>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
