df4f3f520c56d678d0b164d4d601f4b70d8deacf
Prashant Jain <prashantjain0002@gmail.com>
2025-09-19 13:39:17 +0530

    🖼️ Add favicon and 🛠️ fix shadcn/UI bugs

--- frontend/app/root.jsx
+++ frontend/app/root.jsx
@@ -20,6 +20,7 @@ export const links = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+  { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
 ];
 
 export function Layout({ children }) {
