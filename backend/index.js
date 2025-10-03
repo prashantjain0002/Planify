fead98dfb1277d91457b35b45b8514364f059bc3
Prashant Jain <prashantjain0002@gmail.com>
2025-10-03 18:04:47 +0530

    🔧 Fix CORS issues

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: '*',
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
